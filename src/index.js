9c820e768baa17da2a12c0b2fb796c6ea7d4a245
fancyaction <labcoatdisaster@gmail.com>
2018-07-23 13:13:45 -0500

    Fix import links in index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from 'pages/Home';
+import Home from './pages/Home';
 import './index.css';
-import registerServiceWorker from 'utils/registerServiceWorker';
+import registerServiceWorker from './utils/registerServiceWorker';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
 registerServiceWorker();
