14cf55f4c5ddfec23fb23869cce2ac96dfa6e56f
fancyaction <labcoatdisaster@gmail.com>
2018-07-23 18:44:03 -0500

    Add posts data

---
+++ src/pages/home/data.js
@@ -0,0 +1,437 @@
+export default {
+  posts: [
+    {
+      id: 'p_c0b20cc73cec6be3',
+      title: 'Junior Full Stack Web Developer',
+      url:
+        'http://www.indeed.com/rc/clk?jk=c0b20cc73cec6be3&from=vj&pos=bottom',
+      company: 'FasTraxPOS',
+      location: 'Rock Hill, NY',
+      summary:
+        'Is looking for a LOCAL (Rock Hill, NY) Junior Full Stack Web Developer that has exceptional skills to add to our talented creative design team....',
+      date: '3 days ago'
+    },
+    {
+      id: 'p_9b82f0fddefab98c',
+      title: 'Web Developer Internship',
+      url:
+        'http://www.indeed.com/rc/clk?jk=9b82f0fddefab98c&from=vj&pos=bottom',
+      company: 'CFC Associated Builders and Contractors, Inc.',
+      location: 'National, WA',
+      summary:
+        'The Web Developer Intern will assist the Web Services team with designing, building and implementing a new responsive-design website....',
+      date: '30+ days ago'
+    },
+    {
+      id: 'p_fac0e807009d84d6',
+      title: 'Entry Level Web Developer Trainee',
+      url:
+        'http://www.indeed.com/rc/clk?jk=fac0e807009d84d6&from=vj&pos=bottom',
+      company: 'New Equity Productions',
+      location: 'Newport Beach, CA',
+      summary:
+        'Entry-Level Web Developer Trainee. Entry-Level Web Developer Trainee Duties. Entry-Level Web Developer Trainee Must haves:....',
+      date: '28 days ago'
+    },
+    {
+      id: 'p_fa7a5743014bb7db',
+      title: 'Jr- Front End WEB Developer ( Entry Level)',
+      url:
+        'http://www.indeed.com/rc/clk?jk=fa7a5743014bb7db&from=vj&pos=bottom',
+      company: 'Brella Productions',
+      location: 'Evanston, IL 60201',
+      reviews: '6 reviews',
+      summary:
+        'Front End WEB Developer (Entry Level). Brella Productions, a corporate communications company located in Evanston, IL is seeking a Front End Web Developer to...',
+      date: '19 days ago'
+    },
+    {
+      id: 'p_0211048dbffc546c',
+      title: 'WEB DEVELOPER AND CONTENT MANAGER',
+      url:
+        'http://www.indeed.com/rc/clk?jk=0211048dbffc546c&from=vj&pos=bottom',
+      company: 'University of California, Davis',
+      location: 'Davis, CA',
+      reviews: '1,142 reviews',
+      summary:
+        'Experience with PC-based graphic design, web authoring and word processing software, such as Adobe Illustrator, Photoshop, InDesign, and Dreamweaver, to develop...',
+      date: '13 days ago'
+    },
+    {
+      id: 'p_8b2d173a03604a48',
+      title: 'Web Developer',
+      url:
+        'http://www.indeed.com/rc/clk?jk=8b2d173a03604a48&from=vj&pos=bottom',
+      company: 'Amazon.com',
+      location: 'Herndon, VA',
+      reviews: '25,560 reviews',
+      summary:
+        'Experience in system architecture and web application design. Developer/architect for a platform hosting web tools for a variety of data....',
+      date: '3 days ago'
+    },
+    {
+      id: 'p_9ea028c919e0859d',
+      title: 'Web Developer',
+      url:
+        'http://www.indeed.com/rc/clk?jk=9ea028c919e0859d&from=vj&pos=bottom',
+      company: 'TheraNest',
+      location: 'Remote',
+      summary:
+        '2-5 years experience as a web developer, preferably on an IT or Marketing team. Familiarity with responsive web design....',
+      date: '10 days ago'
+    },
+    {
+      id: 'p_d07403bc7a4dd4bd',
+      title: 'Web Developer/Programmer',
+      url:
+        'http://www.indeed.com/rc/clk?jk=d07403bc7a4dd4bd&from=vj&pos=bottom',
+      company: 'Precept Ministries International',
+      location: 'Chattanooga, TN',
+      summary:
+        'Familiarity with web API technology desired. BA/BS from an accredited university with 3 to 5 years of experience working in a web development capacity....',
+      date: '6 hours ago'
+    },
+    {
+      id: 'p_9f6dae7121ff3143',
+      title: 'Web Developer',
+      url:
+        'http://www.indeed.com/rc/clk?jk=9f6dae7121ff3143&from=vj&pos=bottom',
+      company: 'California South Bay University',
+      location: 'Campbell, CA',
+      summary:
+        '2+ years of web development. A solid understanding of web application development processes. California South Bay University is a learning community that seeks...',
+      date: '3 days ago'
+    },
+    {
+      id: 'p_436550941a2039f9',
+      title: 'Young Web Developer',
+      url:
+        'http://www.indeed.com/rc/clk?jk=436550941a2039f9&from=vj&pos=bottom',
+      company: 'Tyle Web Development',
+      location: 'Pittsburgh, PA',
+      summary:
+        'Looking for young Web Developers High School and College age that want to work on web development projects in their spare time....',
+      date: '29 days ago'
+    },
+    {
+      id: 'LDRnhPsL8kz96UKSIKEC8SST_G1VYm4IPnq8f_QOINXUbzM4tpbJGA',
+      url:
+        'https://www.simplyhired.com/job/LDRnhPsL8kz96UKSIKEC8SST_G1VYm4IPnq8f_QOINXUbzM4tpbJGA',
+      title: 'Front End Developer ( REMOTE)',
+      company: 'PharmDl',
+      location: 'Reston, VA',
+      summary:
+        'Collaborate with other developers to integrate front-end code with other technologies including server-side code, APIs and web services, and content management...',
+      date: '2018-06-23T12:09:27Z',
+      salary: '$68,000 - $95,000 a year'
+    },
+    {
+      id: 'WpLllYbf7wJFAXgmvnS1JZMzEcRF9Ln6wNjXpznMyVBs3HQrl3iyMQ',
+      url:
+        'https://www.simplyhired.com/job/WpLllYbf7wJFAXgmvnS1JZMzEcRF9Ln6wNjXpznMyVBs3HQrl3iyMQ',
+      title: 'Senior Web Application Developer',
+      company: 'SRS Computing',
+      location: 'Wexford, PA',
+      summary:
+        'SRS Computing, a highly successful software company is seeking to fill a Full-Time Senior Web Application Developer position at our office in Wexford, PA (...',
+      date: '2018-06-26T15:39:40Z',
+      salary: '$60,000 - $90,000 a year'
+    },
+    {
+      id: 'kV74LDoH0L2-TQqLPqpe7SgjzaCtjqsfUm3Sweia3QWkBYNdgBnKug',
+      url:
+        'https://www.simplyhired.com/job/kV74LDoH0L2-TQqLPqpe7SgjzaCtjqsfUm3Sweia3QWkBYNdgBnKug',
+      title: 'JavaScript Developer',
+      company: 'PF1 Professional Services',
+      location: 'Tampa, FL',
+      summary:
+        'Qualifications for the JavaScript Developer:. Basic understanding of web markup, including HTML5 and CSS3. Looking for a JavaScript Developer to join a team of...',
+      date: '2018-05-14T10:29:44Z',
+      salary: '$70,000 a year'
+    },
+    {
+      id: 'Xyrjrhl0yGMh5r1-IQcFLUXwQ1f2sB_lmvoddENvq8R0kvBDBlULdQ',
+      url:
+        'https://www.simplyhired.com/job/Xyrjrhl0yGMh5r1-IQcFLUXwQ1f2sB_lmvoddENvq8R0kvBDBlULdQ',
+      title: 'Web Developer III',
+      company: 'Seneca Gaming Corporation',
+      location: 'Niagara Falls, NY',
+      summary:
+        'At least six (6) years web application development utilizing OOA/OOD methodologies. Systems analysis, design and construction of web applications, for multiple...',
+      date: '2018-05-08T20:33:30Z',
+      ratings: '3',
+      salary: '$55,000 - $71,000 a year'
+    },
+    {
+      id: 'BV441Iku3joCMgGc7-rebB2gus3i0VnDgNHsKilauhSs9MtZCdgQIA',
+      url:
+        'https://www.simplyhired.com/job/BV441Iku3joCMgGc7-rebB2gus3i0VnDgNHsKilauhSs9MtZCdgQIA',
+      title: 'Website Developer',
+      company: 'Professional Technology Integration, Inc.',
+      location: 'Leesburg, VA',
+      summary:
+        'You are a web developer who can serve as the technical expert on a refresh of our hosted website. Come join a dedicated group of IT professionals in the heart...',
+      date: '10d'
+    },
+    {
+      id: 'spR2vtTYwC6tFeo-xcz_hkA8KzpMBNKD2_rDZipSCTjJQMjEzEncCw',
+      url:
+        'https://www.simplyhired.com/job/spR2vtTYwC6tFeo-xcz_hkA8KzpMBNKD2_rDZipSCTjJQMjEzEncCw',
+      title: 'Front End Developer ( 100% Remote Position)',
+      company: 'Avancer Corporation',
+      location: 'Houston, TX',
+      summary:
+        'Front End Developer. A strong front-end developer with *Backbone.js and Handlebars.js*. A strong front-end developer with *Backbone.js and Handlebars.js*these...',
+      date: '5d',
+      salary: '$90,000 - $100,000 a year'
+    },
+    {
+      id: 'ZYLxGTazpAK9JA6h2AEp6HHBEqNLpRMu8_kiRVB3UGvJI-a5W0kDwg',
+      url:
+        'https://www.simplyhired.com/job/ZYLxGTazpAK9JA6h2AEp6HHBEqNLpRMu8_kiRVB3UGvJI-a5W0kDwg',
+      title: 'Young Web Developer',
+      company: 'Tyle Web Development',
+      location: 'Pittsburgh, PA',
+      summary:
+        'Looking for young Web Developers High School and College age that want to work on web development projects in their spare time....',
+      date: '2018-06-23T21:24:07Z',
+      salary: '$34,000 - $46,000 a year'
+    },
+    {
+      id: 'iMwr7n_v0Ef7OJvJVlrDhrs0PG4gTb8z8kmb_qvZewGmE9A_VySPNQ',
+      url:
+        'https://www.simplyhired.com/job/iMwr7n_v0Ef7OJvJVlrDhrs0PG4gTb8z8kmb_qvZewGmE9A_VySPNQ',
+      title: 'Developer - Entry Level',
+      company: 'Vanguard',
+      location: 'Malvern, PA',
+      summary:
+        '0 \u2013 2 years of developer experience. Are you a developer with a strong growth mindset? We are building Angular web applications and RESTful micro-services using...',
+      date: '2018-06-12T12:51:26Z',
+      ratings: '3.8',
+      salary: '$68,000 - $95,000 a year'
+    },
+    {
+      id: 'nDLtGtOU_e1HJMJHQZ7RIKYQsWS7g7V7jT-Z65vMXc-WK4kWaKwiUQ',
+      url:
+        'https://www.simplyhired.com/job/nDLtGtOU_e1HJMJHQZ7RIKYQsWS7g7V7jT-Z65vMXc-WK4kWaKwiUQ',
+      title: 'HTML Developer',
+      company: 'TPR Education, LLC',
+      location: 'New York, NY',
+      summary:
+        'TPR/Tutor.com HTML Developer. Server-side development and experience working on a high-volume web site. 1+ year of experience implementing web sites with...',
+      date: '2018-06-10T11:02:36Z',
+      ratings: '4.5',
+      salary: '$61,000 - $85,000 a year'
+    },
+    {
+      id: '_R-1P93H5785iPce5hzaKQpFnUfSsRZKCGM3vZ_WW7B0xK3rVIPyig',
+      url:
+        'https://www.simplyhired.com/job/_R-1P93H5785iPce5hzaKQpFnUfSsRZKCGM3vZ_WW7B0xK3rVIPyig',
+      title: 'Front-End Developer',
+      company: 'TeisoftLLC',
+      location: 'Doral, FL',
+      summary:
+        'Strong knowledge in *_Responsive web design_*. Key skills required for the job are:....',
+      date: '2018-06-27T16:45:40Z',
+      salary: '$50 an hour'
+    },
+    {
+      id: '4mB2CywawWEUQw5d7v-zJGZAg26xckJgSyDGKoomTJpRrG3ipKyxmA',
+      url:
+        'https://www.simplyhired.com/job/4mB2CywawWEUQw5d7v-zJGZAg26xckJgSyDGKoomTJpRrG3ipKyxmA',
+      title: 'Junior Software Developer',
+      company: 'Hatch',
+      location: 'Chicago, IL',
+      summary:
+        "Basic understanding of how web APIs and browser-based web applications work. We're a small, skilled team of experienced agile web software developers building...",
+      date: '2018-07-09T14:37:36Z',
+      ratings: '4',
+      salary: '$77,000 - $100,000 a year'
+    },
+    {
+      id: 'rbFvdSeCGju40AgJrBzkFA6aEvQ3TveK0C49njaEtKmhgVBm_TV74g',
+      url:
+        'https://www.simplyhired.com/job/rbFvdSeCGju40AgJrBzkFA6aEvQ3TveK0C49njaEtKmhgVBm_TV74g',
+      title: 'Junior Web Developer',
+      company: 'Principle',
+      location: 'Knoxville, TN',
+      summary:
+        'Maintain web application forms, data groups, and web user experience using third-party web portal framework software....',
+      date: '2018-06-28T03:06:56Z',
+      ratings: '4.4',
+      salary: '$72,000 - $99,000 a year'
+    },
+    {
+      id: 'NQluqOmpY005g45FJwgayZkhV0XlJnV9hgV53k8W85uk2z2hOX7j9Q',
+      url:
+        'https://www.simplyhired.com/job/NQluqOmpY005g45FJwgayZkhV0XlJnV9hgV53k8W85uk2z2hOX7j9Q',
+      title: 'Jr. Software Developer',
+      company: 'Streamlinx',
+      location: 'Hudson, OH',
+      summary:
+        'Junior Software Developer. If you\u2019re a Junior Software Developer and want to be part of something truly special, read on....',
+      date: '12d',
+      salary: '$58,000 - $81,000 a year'
+    },
+    {
+      id: 'aQHH_eRhws3ZwtIO3R2Om3SDlmXaAr5hVr3eT7WMntsxX2TG1vNT3Q',
+      url:
+        'https://www.simplyhired.com/job/aQHH_eRhws3ZwtIO3R2Om3SDlmXaAr5hVr3eT7WMntsxX2TG1vNT3Q',
+      title: 'Web Developer',
+      company: 'Amazon.com',
+      location: 'Herndon, VA',
+      summary:
+        'Experience in system architecture and web application design. Developer/architect for a platform hosting web tools for a variety of data....',
+      date: '3d',
+      ratings: '3.7'
+    },
+    {
+      id: 'ZJGqtABQpO77NS59B0_Yrz-Drsj5GhZG9PP2gr3k4PiLl94E9dB9pw',
+      url:
+        'https://www.simplyhired.com/job/ZJGqtABQpO77NS59B0_Yrz-Drsj5GhZG9PP2gr3k4PiLl94E9dB9pw',
+      title: 'Jr. Front End Programmer',
+      company: 'New York Film Academy',
+      location: 'New York, NY',
+      summary:
+        'Work with developer to test new features of the web pages. Proven working experience in QA for web. Cooperate with web designers to match visual design intent....',
+      date: '2018-07-03T20:55:19Z',
+      salary: '$40,000 - $45,000 a year'
+    },
+    {
+      id: 'IT7Ks58cpT2TXBeUUZ9H_KuK8JsLD4e02mgX31jgKpjvsbG8XcLJyw',
+      url:
+        'https://www.simplyhired.com/job/IT7Ks58cpT2TXBeUUZ9H_KuK8JsLD4e02mgX31jgKpjvsbG8XcLJyw',
+      title: 'Front-End Website Developer in WordPress and HTML',
+      company: 'Website Development and Internet Marketing Firm',
+      location: 'San Jose, CA',
+      summary:
+        'We are looking for an enthusiastic and highly motivated web developer to maintain our current websites and build new websites....',
+      date: '2018-07-05T16:18:11Z',
+      salary: '$16 - $22 an hour'
+    },
+    {
+      id: 'RU2C1TwYCTYE2RVA_1hlBVUAFJ6kiIq_KWGLob6OOAhxb4Lom9CTJg',
+      url:
+        'https://www.simplyhired.com/job/RU2C1TwYCTYE2RVA_1hlBVUAFJ6kiIq_KWGLob6OOAhxb4Lom9CTJg',
+      title: 'Web Content Coordinator-Marketing & Communications',
+      company: 'Johns Hopkins Hospital/Johns Hopkins Health System',
+      location: 'Baltimore, MD',
+      summary:
+        'Web Content Coordinator-Marketing &amp; Communications. Working knowledge and experience using content management systems, social media tools and related Web...',
+      date: '2018-05-15T12:55:46Z',
+      ratings: '4.1',
+      salary: '$38,000 - $50,000 a year'
+    },
+    {
+      id: 'DmNvMyO-NgmlGDnE-YWxlBrePU9Kw6CIqAEEAwcNP3i7Sdy55Fn9EA',
+      url:
+        'https://www.simplyhired.com/job/DmNvMyO-NgmlGDnE-YWxlBrePU9Kw6CIqAEEAwcNP3i7Sdy55Fn9EA',
+      title: 'Front End Web Developer',
+      company: 'Kaplan',
+      location: 'Wisconsin',
+      summary:
+        'Provide website maintenance and enhancements and work with developers on development/maintenance of web pages....',
+      date: '13d',
+      ratings: '3.7',
+      salary: '$53,000 - $77,000 a year'
+    },
+    {
+      id: 'Cej0-cW9xI93TSkIRt5tzcQJawYaoaSoSvUx_dFmv9C4EjSKUrxT1Q',
+      url:
+        'https://www.simplyhired.com/job/Cej0-cW9xI93TSkIRt5tzcQJawYaoaSoSvUx_dFmv9C4EjSKUrxT1Q',
+      title: 'Intern- Full Stack Software Engineer',
+      company: 'Symplicity Corporation',
+      location: 'Arlington, VA',
+      summary:
+        'We\u2019re seeking a Full Stack Engineer Intern to join a team of product specialists, developers and designers in building industry-leading web applications....',
+      date: '2018-05-22T00:39:15Z',
+      ratings: '3.3',
+      salary: '$91,000 - $120,000 a year'
+    },
+    {
+      id: 'BrkXd7DcXmWl9X_bKYLx5I_Ca42tmDY7zvGnJiOSUI55uR5COrzcWA',
+      url:
+        'https://www.simplyhired.com/job/BrkXd7DcXmWl9X_bKYLx5I_Ca42tmDY7zvGnJiOSUI55uR5COrzcWA',
+      title: 'Frontend Developer - Los Angeles',
+      company: 'Quintessentially Inc',
+      location: 'Los Angeles, CA',
+      summary:
+        'Strong web development skills including CSS, HTML, Java, JavaScript, AJAX, JSON, XML, web services. The ideal candidate will have strong web development skills...',
+      date: '2d',
+      salary: '$70,000 - $88,000 a year'
+    },
+    {
+      id: 'AZ4_mPvr96mjoyt6vNc7k9rTXs6_dCyxmsOJLXfiSt9bSxEcNYS4Ag',
+      url:
+        'https://www.simplyhired.com/job/AZ4_mPvr96mjoyt6vNc7k9rTXs6_dCyxmsOJLXfiSt9bSxEcNYS4Ag',
+      title: 'Frontend Developer',
+      company: 'Function of Beauty',
+      location: 'New York, NY',
+      summary:
+        'The front-end developer will also be responsible for creating solutions to web design issues and assist in the upkeep and usability of the website....',
+      date: '11d',
+      salary: '$68,000 - $91,000 a year'
+    },
+    {
+      id: 'iwBTWOzxTTvs8w6S7mNnkY9giGe58IC5H4BtfJm-CFXJt9lYtxTbsw',
+      url:
+        'https://www.simplyhired.com/job/iwBTWOzxTTvs8w6S7mNnkY9giGe58IC5H4BtfJm-CFXJt9lYtxTbsw',
+      title: 'Frontend Developer',
+      company: 'Orion Network Solutions, Inc.',
+      location: "Coeur d'Alene, ID",
+      summary:
+        'Any Web Mapping Service (WMS) experience. Orion Network Solutions is looking for a skilled professional to join our development team....',
+      date: '12d',
+      salary: '$69,000 - $93,000 a year'
+    },
+    {
+      id: 'ZWiajdEqxP5IYdeZJX5HXWCUznFPLW3W-2Ah7y8t2Ip3nA60dWIJKw',
+      url:
+        'https://www.simplyhired.com/job/ZWiajdEqxP5IYdeZJX5HXWCUznFPLW3W-2Ah7y8t2Ip3nA60dWIJKw',
+      title: 'Frontend Developer',
+      company: 'Publishers Clearing House',
+      location: 'Jericho, NY',
+      summary:
+        'To help develop mobile and desktop web sites using cutting edge web technologies for all our business platforms....',
+      date: '2018-06-27T14:20:19Z',
+      ratings: '4.2',
+      salary: '$77,000 - $99,000 a year'
+    },
+    {
+      id: 'nKpOgiMnWYzWLj02Mf3HtNPvGWzHTxSjMjqcJBE0eHecRVNjl1JJNg',
+      url:
+        'https://www.simplyhired.com/job/nKpOgiMnWYzWLj02Mf3HtNPvGWzHTxSjMjqcJBE0eHecRVNjl1JJNg',
+      title: 'UI/UX Developer - Local Candidates Only',
+      company: 'Webbula',
+      location: 'Wexford, PA',
+      summary:
+        'We are looking for an experienced UI/UX designer and developer to join our team to enhance our end users experience to our industry leading web applications....',
+      date: '13d',
+      salary: '$50,000 - $80,000 a year'
+    },
+    {
+      id: 'Er8k2PH6DwYWlF8SfNkw3gRV62h9sfshyPjR95lT08HCn5rtEJwR4g',
+      url:
+        'https://www.simplyhired.com/job/Er8k2PH6DwYWlF8SfNkw3gRV62h9sfshyPjR95lT08HCn5rtEJwR4g',
+      title: 'Senior User Interface Engineer',
+      company: 'iCrossing',
+      location: 'Dallas, TX',
+      summary:
+        '3rd-party API integration via REST-ful web services. We bridge the creative and technical parts of web development....',
+      date: '5d',
+      ratings: '3.1'
+    },
+    {
+      id: 'L8f5_Kv3Xfwc6TX98uNXj6AzYnzR6MlbUSUioJLOipCJ6rAwNJeW7A',
+      url:
+        'https://www.simplyhired.com/job/L8f5_Kv3Xfwc6TX98uNXj6AzYnzR6MlbUSUioJLOipCJ6rAwNJeW7A',
+      title: 'PHP/JavaScript Developer',
+      company: 'Iredale Mineral Cosmetics Ltd.',
+      location: 'Great Barrington, MA',
+      summary:
+        'We are looking for a talented and efficient PHP and JavaScript developer to join our small development team to help write solid, DRY, fully-tested, object...',
+      date: '2018-05-31T15:45:33Z',
+      salary: '$80,000 - $110,000 a year'
+    }
+  ]
+};
