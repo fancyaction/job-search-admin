a711fd64d36a8cec120b7886bf1284d342cad7bf
fancyaction <labcoatdisaster@gmail.com>
2018-07-23 18:43:51 -0500

    Setup homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
-import simpleRestProvider from 'ra-data-simple-rest';
+import posts from './home/Posts';
+import dataProvider from './home/dataProvider';
 
-const dataProvider = simpleRestProvider(
-  'https://indreed.herokuapp.com/api/jobs/?q=web+developer&limit=50'
+const Home = () => (
+  <Admin title="Your Custom Job Search 💼" dataProvider={dataProvider}>
+    <Resource name="posts" {...posts} />
+  </Admin>
 );
-const Home = () => <Admin dataProvider={dataProvider} />;
 
 export default Home;
