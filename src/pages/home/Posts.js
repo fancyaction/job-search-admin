e14bec984f265b7d129f9d981dd0983582607cb1
fancyaction <labcoatdisaster@gmail.com>
2018-07-23 19:09:35 -0500

    Add Create, Edit to Posts.js

--- src/pages/home/Posts.js
+++ src/pages/home/Posts.js
@@ -1,7 +1,11 @@
 import BookIcon from '@material-ui/icons/Book';
 import PostList from './posts/PostList';
+import PostEdit from './posts/PostEdit';
+import PostCreate from './posts/PostCreate';
 
 export default {
   list: PostList,
+  edit: PostEdit,
+  create: PostCreate,
   icon: BookIcon
 };
